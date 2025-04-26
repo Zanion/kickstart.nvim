b8d240cd5b0a04790fa9bb435ae35f04ee814250
orip <oriori1703@gmail.com>
2025-04-26 14:35:04 +0300

    Add type hints to plugin options where possible
    This could help beginners to get autocompletion, catch mistakes earlier,
    and allow them to skip the docs for simple configs.
    
    This is not perfect because a lot of the plugins type all of their keys
    as required, even though they have defaults, but this is good enough.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,3 +1,10 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+---@module 'lazy'
+---@type LazySpec
 return {
-  { "nvim-tree/nvim-web-devicons", opts = {} },
+  { 'nvim-tree/nvim-web-devicons', opts = {} },
 }
