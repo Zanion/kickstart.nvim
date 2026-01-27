465cd6d3bfe26c68a8abc70b554e9a78d18c01cf
Advin Suryavanshi <78780005+advinsuryavanshi@users.noreply.github.com>
2026-01-27 19:47:34 +0530

    fix: update main module reference for nvim-treesitter (#1832)
    configs -> config in init.lua

--- init.lua
+++ init.lua
@@ -942,7 +942,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     branch = 'master',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter.config', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'rust', 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
