193066bf346f834ccbbff7c9e838edbf75bec773
TJ DeVries <devries.timothyj@gmail.com>
2026-01-27 12:00:56 -0500

    fix: trimming down config and updating stylua

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -26,20 +26,40 @@ return {
   },
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<F5>', require('dap').continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<F1>', require('dap').step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<F2>', require('dap').step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<F3>', require('dap').step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>b', require('dap').toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>B', function()
-      require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-    end, { desc = 'Debug: Set Breakpoint' })
+    {
+      '<F5>',
+      function() require('dap').continue() end,
+      desc = 'Debug: Start/Continue',
+    },
+    {
+      '<F1>',
+      function() require('dap').step_into() end,
+      desc = 'Debug: Step Into',
+    },
+    {
+      '<F2>',
+      function() require('dap').step_over() end,
+      desc = 'Debug: Step Over',
+    },
+    {
+      '<F3>',
+      function() require('dap').step_out() end,
+      desc = 'Debug: Step Out',
+    },
+    {
+      '<leader>b',
+      function() require('dap').toggle_breakpoint() end,
+      desc = 'Debug: Toggle Breakpoint',
+    },
+    {
+      '<leader>B',
+      function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end,
+      desc = 'Debug: Set Breakpoint',
+    },
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     {
       '<F7>',
-      function()
-        require('dapui').toggle()
-      end,
+      function() require('dapui').toggle() end,
       desc = 'Debug: See last session result.',
     },
   },
